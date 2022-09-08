f28ea4e3168a8415ee605f16b9c3c8324662316e
kostya <kostizavali@gmail.com>
2022-09-08 15:08:06 +0300

    2 tasks

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -11,6 +11,7 @@ const refs = {
 };
 let goalDate;
 let timerId = null;
+refs.startElem.disabled = true;
 
 const options = {
   enableTime: true,
@@ -18,16 +19,57 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    Notiflix.Notify.success(
-      `Все супер таймер запущено, таймер зупиниться в  ${selectedDates}`
-    );
+    if (timerId) {
+      Notiflix.Notify.warning('будь ласка, оновіть сторінку');
+      return;
+    }
+    if (Date.now() > selectedDates[0]) {
+      Notiflix.Notify.failure(
+        'ще не придумали машину часу, вибіріть дату в майбутньому'
+      );
+      return;
+    }
+    console.log(selectedDates[0]);
+    goalDate = selectedDates[0];
   },
-  onOpen() {
-    Notiflix.Notify.info('Будь ласка, виберіть дату');
+  onChange(selectedDates) {
+    if (Date.now() > selectedDates[0]) {
+      const day = Date.now();
+      refs.startElem.disabled = true;
+
+      Notiflix.Notify.failure(
+        'ще не придумали машину часу, вибіріть дату в майбутньому'
+      );
+    } else if (Date.now() < selectedDates[0]) {
+      refs.startElem.disabled = false;
+    }
   },
-  minDate: 'today',
 };
 flatpickr(refs.inputElem, options);
+
+refs.startElem.addEventListener('click', () => {
+  startTimer();
+  refs.startElem.disabled = true;
+});
+
+function startTimer() {
+  Notiflix.Notify.success('відлік почався');
+  timerId = setInterval(() => {
+    const difTime = goalDate - Date.now();
+    const { days, hours, minutes, seconds } = convertMs(difTime);
+    refs.daysElem.textContent = addLeadingZero(days);
+    refs.hoursElem.textContent = addLeadingZero(hours);
+    refs.minutesElem.textContent = addLeadingZero(minutes);
+    refs.secondsElem.textContent = addLeadingZero(seconds);
+
+    if (days === 0 && hours === 0 && minutes === 0 && seconds === 0) {
+      clearInterval(timerId);
+      refs.startElem.disabled = false;
+      Notiflix.Notify.info('час сі скінчив');
+      timerId = null;
+    }
+  }, 1000);
+}
 function convertMs(ms) {
   const second = 1000;
   const minute = second * 60;
@@ -42,18 +84,9 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-// console.log(convertMs(2000)); {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); {days: 0, hours: 6 minutes: 42, seconds: 20}
+// // console.log(convertMs(2000)); {days: 0, hours: 0, minutes: 0, seconds: 2}
+// // console.log(convertMs(140000)); {days: 0, hours: 0, minutes: 2, seconds: 20}
+// // console.log(convertMs(24140000)); {days: 0, hours: 6 minutes: 42, seconds: 20}
 function addLeadingZero(value) {
   return value.toString().padStart(2, '0');
 }
-// refs.startElem.addEventListener('click', () => {
-//   Notiflix.Notify.success('Все добре');
-
-//   Notiflix.Notify.failure('Все дуже погано');
-
-//   Notiflix.Notify.warning('Уважно передивись код');
-
-//   Notiflix.Notify.info('Інформація');
-// });
